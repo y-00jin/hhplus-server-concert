a7603a50a09f4b9e1d51981ca54714e42cb158b3
Yoojin <81798918+y-00jin@users.noreply.github.com>
2025-06-10 01:46:23 +0900

    CHORE: H2 DB 테스트 의존성 추가 및 JPA ddl-auto 설정 변경
    - testImplementation("com.h2database:h2") 추가하여 테스트 환경에서 H2 데이터베이스 사용
    - application.yml JPA ddl-auto 옵션을 'update'로 수정하여 테스트 시 테이블 자동 생성 및 업데이트 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:mysql")
+	testImplementation("com.h2database:h2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.withType<Test> {
