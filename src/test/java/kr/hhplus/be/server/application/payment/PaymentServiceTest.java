78c9daac96e5a43cbb2518636bdc22b0913e6539
yjlee <edbwls2@naver.com>
2025-07-09 16:21:15 +0900

    TEST: 결제 트랜잭션 분리 및 Soldout 랭킹 이벤트 구현에 따른 테스트 코드 수정 (#42)

--- src/test/java/kr/hhplus/be/server/application/payment/PaymentServiceTest.java
+++ src/test/java/kr/hhplus/be/server/application/payment/PaymentServiceTest.java
@@ -1,17 +1,15 @@
 package kr.hhplus.be.server.application.payment;
 
 import kr.hhplus.be.server.common.exception.ApiException;
-import kr.hhplus.be.server.domain.concert.*;
+import kr.hhplus.be.server.domain.concert.Seat;
+import kr.hhplus.be.server.domain.concert.SeatStatus;
 import kr.hhplus.be.server.domain.lock.DistributedLockRepository;
 import kr.hhplus.be.server.domain.payment.Payment;
-import kr.hhplus.be.server.domain.payment.PaymentRepository;
 import kr.hhplus.be.server.domain.payment.PaymentStatus;
-import kr.hhplus.be.server.domain.queue.QueueTokenRepository;
 import kr.hhplus.be.server.domain.reservation.ReservationStatus;
 import kr.hhplus.be.server.domain.reservation.SeatReservation;
 import kr.hhplus.be.server.domain.reservation.SeatReservationRepository;
 import kr.hhplus.be.server.domain.user.User;
-import kr.hhplus.be.server.domain.user.UserBalanceRepository;
 import kr.hhplus.be.server.domain.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,7 +18,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -32,16 +29,11 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class PaymentServiceTest {
 
-    @Mock PaymentRepository paymentRepository;
     @Mock SeatReservationRepository seatReservationRepository;
     @Mock UserRepository userRepository;
-    @Mock UserBalanceRepository userBalanceRepository;
-    @Mock SeatRepository seatRepository;
-    @Mock QueueTokenRepository queueTokenRepository;
     @Mock DistributedLockRepository distributedLockRepository;
 
-    @Mock ConcertSoldoutRankingRepository concertSoldoutRankingRepository;
-    @Mock ConcertScheduleRepository scheduleRepository;
+    @Mock PaymentTransactionalService paymentTransactionalService;
 
     @InjectMocks
     PaymentService paymentService;
@@ -51,7 +43,6 @@ class PaymentServiceTest {
     Long seatId = 1L;
     Long scheduleId = 1L;
     int price = 30000;
-    long currentBalance = 50000L;
 
     User user;
     Seat seat;
@@ -76,24 +67,8 @@ class PaymentServiceTest {
                     Supplier<?> supplier = invocation.getArgument(1);
                     return supplier.get();
                 });
-        when(seatReservationRepository.findById(reservationId)).thenReturn(Optional.of(reservation));  // 예약 조회
-        when(seatRepository.findById(seatId)).thenReturn(Optional.of(seat));   // 좌석 조회
-        when(userBalanceRepository.findCurrentBalanceByUserId(userId)).thenReturn(currentBalance); // 잔액 조회
-        when(userBalanceRepository.save(any())).thenAnswer(inv -> inv.getArgument(0));
-        when(paymentRepository.save(any())).thenReturn(payment);
-        when(queueTokenRepository.findTokenIdByUserIdAndScheduleId(userId, scheduleId)).thenReturn(Optional.empty());
 
-        // 매진 조건 세팅 (전체 좌석 = CONFIRMED 좌석)
-        when(seatRepository.countByConcertSchedule_ScheduleId(scheduleId)).thenReturn(1);
-        when(seatRepository.countByConcertSchedule_ScheduleIdAndStatus(scheduleId, SeatStatus.CONFIRMED)).thenReturn(1);
-
-        // 아직 랭킹 등록 안 됨
-        when(concertSoldoutRankingRepository.isAlreadyRanked(anyString(), eq(scheduleId))).thenReturn(false);
-        // 콘서트 생성일 반환
-        when(scheduleRepository.findById(scheduleId)).thenReturn(
-                Optional.of(new ConcertSchedule(scheduleId, LocalDate.now().plusDays(1),  LocalDateTime.now().minusMinutes(10), LocalDateTime.now().minusMinutes(10)))
-        );
-
+        when(paymentTransactionalService.doPaymentTransactional(anyLong(), anyLong(), anyLong())).thenReturn(payment);
 
         // when
         Payment result = paymentService.payment(userId, reservationId);
@@ -105,11 +80,6 @@ class PaymentServiceTest {
         assertThat(result.getAmount()).isEqualTo(price);
         assertThat(result.getStatus()).isEqualTo(PaymentStatus.SUCCESS);
 
-        verify(userBalanceRepository).save(any());
-        verify(seatReservationRepository).save(any());
-        verify(seatRepository).save(any());
-        verify(paymentRepository).save(any());
-        verify(concertSoldoutRankingRepository, times(1)).addSoldoutRanking(anyString(), eq(scheduleId), anyDouble());
     }
 
     @Test
@@ -121,34 +91,4 @@ class PaymentServiceTest {
         assertThatThrownBy(() -> paymentService.payment(userId, reservationId))
                 .isInstanceOf(ApiException.class);
     }
-
-    @Test
-    void 만료된_예약은_예외() {
-        // given
-        SeatReservation expiredReservation = new SeatReservation(
-                reservationId, userId, 10L,
-                ReservationStatus.TEMP_RESERVED,
-                LocalDateTime.now().minusMinutes(1),  // 만료
-                LocalDateTime.now().minusMinutes(6),
-                LocalDateTime.now()
-        );
-
-        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-        when(distributedLockRepository.withMultiLock(anyList(), any(), anyLong(), anyInt(), anyLong()))
-                .thenAnswer(invocation -> {
-                    // 실제 람다 실행
-                    Supplier<?> supplier = invocation.getArgument(1);
-                    return supplier.get();
-                });
-        when(seatReservationRepository.findById(reservationId)).thenReturn(Optional.of(expiredReservation));
-
-        // then
-        assertThatThrownBy(() -> paymentService.payment(userId, reservationId))
-                .isInstanceOf(ApiException.class);
-    }
-
-
-
-
-
 }
